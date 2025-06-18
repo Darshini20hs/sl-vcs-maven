b3172527b2bd06d312e516f2e69f587af088b068
Gurudrshini sree <gurudrshini.sree@multichoice.co.za>
2025-06-18 16:05:38 +0530

    commit 5

--- src/main/java/mypkg/calci.java
+++ src/main/java/mypkg/calci.java
@@ -7,7 +7,9 @@ public class calci {
 	int diff(int a, int b) {
 		return a-b;
 	}
-  
+	int mul(int a, int b) {
+		return a-b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -15,6 +17,7 @@ public class calci {
 		calci calc = new calci();
 		System.out.println(calc.add(10,20));
 		System.out.println(calc.diff(20,10));
+		System.out.println(calc.mul(20,10));
 	}
 
 }
